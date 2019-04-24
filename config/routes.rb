680a4ba9a2dcc01f68571188bd17bf13f669872d
davask <contact@davask.com>
2019-04-24 11:40:44 +0200

    Update routes settings

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ RedmineApp::Application.routes.draw do
   get 'schedules/projects', :controller => 'schedules', :action => 'projects'
   get 'my/schedule', :controller => 'schedules', :action => 'my_index'
   get 'account/schedule/:user_id', :controller => 'schedules', :action => 'index'
-  get 'account/schedule/:user_id/default', :controller => 'schedules', :action => 'default'
-  get 'account/schedule/:user_id/edit', :controller => 'schedules', :action => 'edit'
+  match 'account/schedule/:user_id/default', :controller => 'schedules', :action => 'default', :via => [:get, :post]
+  match 'account/schedule/:user_id/edit', :controller => 'schedules', :action => 'edit', :via => [:get, :post]
   get 'projects/:project_id/schedules', :controller => 'schedules', :action => 'index'
   get 'projects/:project_id/schedules/details', :controller => 'schedules', :action => 'details'
-  get 'projects/:project_id/schedules/edit', :controller => 'schedules', :action => 'edit'
+  match 'projects/:project_id/schedules/edit', :controller => 'schedules', :action => 'edit', :via => [:get, :post]
   get 'projects/:project_id/schedules/fill', :controller => 'schedules', :action => 'fill'
 end
