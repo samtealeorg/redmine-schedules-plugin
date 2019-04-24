9ee45d676ea488242cccf34cd18223040e60786b
davask <contact@davask.com>
2019-04-24 15:36:23 +0200

    Update mailer settings

--- app/models/schedule_mailer.rb
+++ app/models/schedule_mailer.rb
@@ -1,7 +1,8 @@
-class ScheduleMailer < Mailer
-    def future_changed(user_source, user_target, project, date, hours)
-        recipients user_target.mail
-        subject "Schedule changed"
+class ScheduleMailer < ActionMailer::Base
+    default from: "tickets@davask.com"
+    def deliver_future_changed(user_source, user_target, project, date, hours)
+        # recipients user_target.mail
+        # subject "Schedule changed"
 
         body(
             :user => user_source,
@@ -9,5 +10,7 @@ class ScheduleMailer < Mailer
             :date => date,
             :hours => hours
         )
+
+        mail(to: user_target.mail)
     end
 end
